642ac46cfb21594a4b9a92f7014936cfe4e4808f
Tyler <itsteccs@gmail.com>
2024-09-12 23:26:03 -0500

    remove unused line

--- day4/part1.py
+++ day4/part1.py
@@ -2,12 +2,10 @@ with open('input.txt') as f:
     lines = f.readlines()
 
 sum = 0
-
 for line in lines:
-    points = 0
-    line.split(":")[1].strip().split("|")[0].strip().split(" ")
     winning_nums = list(filter(None, line.split(":")[1].strip().split("|")[0].strip().split(" ")))
     my_nums = list(filter(None, line.split(":")[1].strip().split("|")[1].strip().split(" ")))
+    points = 0
 
     for num in my_nums:
         if num in winning_nums:
